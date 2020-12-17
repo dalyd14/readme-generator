63b73f057c7062bb67b67c23ee1ca7d630403a53
David Daly <dalyd14@gmail.com>
2020-12-17 12:06:03 -0500

    deleted some files and installed inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,19 @@
+const inquirer = require("inquirer");
+
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
